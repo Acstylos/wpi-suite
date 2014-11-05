8bc92db186e63571ac653242ca010e537b5a4760
Stylos <a.stylos983@Gmail.com>
2014-11-04 20:34:58 -0500

    An attempt to fix gerrit/github connect with a comment!
    Change-Id: If67096eae7ec2dbb1cb90551b21d9648c988d18a

--- TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/TaskManager.java
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/TaskManager.java
@@ -20,7 +20,7 @@ import javax.swing.JProgressBar;
 
 import edu.wpi.cs.wpisuitetng.janeway.modules.IJanewayModule;
 import edu.wpi.cs.wpisuitetng.janeway.modules.JanewayTabModel;
-
+// Comment to check Gerrit
 /**
  * This is the main class for the WPI Suite TM module for Janeway.
  *
