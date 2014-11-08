5ade154c35a3b6472b0f5759fa1a7a1dcec21cdb
Stylos <a.stylos983@Gmail.com>
2014-11-08 17:10:48 -0500

    Add inital MainView and subcomponents for TaskManger.
    This includes the workflow view "BucketView" and the Panels that will act as boards "BucketPanel".
    
    Change-Id: I5939c3a00571315149f53b9c237a75fe081159e3

---
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/workflow/WorkflowView.java
@@ -0,0 +1,70 @@
+package edu.wpi.cs.wpisuitetng.modules.taskmanager.view.workflow;
+
+import javax.swing.JPanel;
+import javax.swing.BoxLayout;
+import javax.swing.border.TitledBorder;
+
+import java.awt.Dimension;
+import java.util.List;
+
+/**
+ * WorkflowView is the panel that holds the list of buckets that represents the
+ * full workflow of the project. The title of the workflow corresponds to the
+ * project name the workflow is for.  
+ * @author Thefloorisjava
+ *
+ */
+public class WorkflowView extends JPanel
+{
+
+    private static final long serialVersionUID = -5937582878085666950L;
+    private String title;
+    private List<BucketView> bucketViews;
+
+    /**
+     * Constructor for the panel that holds the workflow of buckets.
+     * @param title String that defines how a bucket will be titled.
+     */
+    public WorkflowView(String title) {
+        /* Buckets will be created left to right, never on top 
+         * of each other.
+         */
+        setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+        setBorder(new TitledBorder(null, title, TitledBorder.LEADING, TitledBorder.TOP, null, null));
+    }
+
+    /**
+     * @return A list of BucketViews corresponding to the buckets in the workflow process.
+     */
+    public List<BucketView> getBucketViews() {
+        return bucketViews;
+    }
+
+    /**
+     * @return A string defining how a bucket will be titled.
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param buckets List of buckets corresponding to the buckets in the workflow process.
+     */
+    public void setBucketViews(List<BucketView> buckets){
+        this.bucketViews = buckets;
+        for(BucketView bucket: bucketViews){
+            bucket.setPreferredSize(new Dimension(250, 500));
+            bucket.setMinimumSize(new Dimension(250, 500));
+            bucket.setMaximumSize(new Dimension(500, 700));
+            add(bucket);
+        }
+    }
+
+    /**
+     * @param title A string defining how a bucket is titled.
+     */
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+}
