ef83b231da8f19dfeb0f6fab979fd4305945a953
magellantoo <lambert.ww@gmail.com>
2014-11-10 22:57:49 -0500

    Removes previous workflow presenters and adds presenters for the BucketView and WorkflowView. Moves the construction of the 4 static buckets to the WorkflowViewPresenter

---
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/presenter/WorkflowPresenter.java
@@ -0,0 +1,56 @@
+/*
+ * 
+ * 
+ * 
+ */
+
+
+package edu.wpi.cs.wpisuitetng.modules.taskmanager.presenter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.wpi.cs.wpisuitetng.modules.taskmanager.view.workflow.BucketView;
+import edu.wpi.cs.wpisuitetng.modules.taskmanager.view.workflow.WorkflowView;
+
+/**
+ * Creates 4 static buckets for the workflow and passes it to the view
+ * 
+ * @author TheFloorIsJava
+ * 
+ */
+
+public class WorkflowPresenter{
+	
+	private WorkflowView view;
+	
+	/**
+	 * Constructor currently does nothing right now
+	 * 
+	 * @param view The view associated with this presenter
+	 */
+	public WorkflowPresenter(WorkflowView view) {
+		this.view = view;
+	}
+	
+	/**
+	 * Creates 4 static bucket views to add to the view
+	 * Then returns the list of BucketViews associated with view
+	 * 
+	 * @return List of BucketViews to be displayed in the view
+	 */
+	public List<BucketView> getBucketViews(){
+
+        List<BucketView> bucketList = new ArrayList<BucketView>();
+        BucketView bucket1 = new BucketView("New");
+        BucketView bucket2 = new BucketView("Scheduled");
+        BucketView bucket3 = new BucketView("In Progress");
+        BucketView bucket4 = new BucketView("Completed");
+        bucketList.add(bucket1);
+        bucketList.add(bucket2);
+        bucketList.add(bucket3);
+        bucketList.add(bucket4);
+		
+		return bucketList;
+	}
+}
