2b4d6552394a20eae8ba30a8319318141ea13acb
magellantoo <lambert.ww@gmail.com>
2014-11-12 01:19:03 -0500

    Added networking for the Workflow and Buckets and also hard coded 4 buckets in the main workflow.
    Removed empty, BucketModelList, and WorkflowModelEntityManager. Replaced with working versions of WorkflowEntityManager and updated ManagerLayer.
    
    Added BucketObserver and fixed WorkflowObserver and also fixed the presenters for the workflow and buckets.
    
    Moved Views to their proper package.
    
    Allows anyone to create the four hard-coded buckets without any prior database information
    
    Added comments and made several classes more uniform
    
    Change-Id: I3d6e1bf43265cf3f1d339141fe149a5d6fe1d5ad

---
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/presenter/WorkflowObserver.java
@@ -0,0 +1,104 @@
+package edu.wpi.cs.wpisuitetng.modules.taskmanager.presenter;
+
+import edu.wpi.cs.wpisuitetng.network.RequestObserver;
+import edu.wpi.cs.wpisuitetng.network.models.HttpMethod;
+import edu.wpi.cs.wpisuitetng.network.models.IRequest;
+import edu.wpi.cs.wpisuitetng.network.models.ResponseModel;
+import edu.wpi.cs.wpisuitetng.modules.taskmanager.model.WorkflowModel;
+
+/**
+ * 
+ * 
+ * @author TheFloorIsJava
+ *
+ */
+public class WorkflowObserver implements RequestObserver {
+
+    private WorkflowPresenter presenter;
+    private HttpMethod method;
+
+    public WorkflowObserver(WorkflowPresenter presenter, HttpMethod method) {
+        this.presenter = presenter;
+        this.method = method;
+    }
+
+    /**
+     * Parse the something from the response received by the network
+     * 
+     */
+    @Override
+    public void responseSuccess(IRequest iReq) {
+        System.err.println(iReq.getResponse().getBody() + method);
+        // Store the response
+        final ResponseModel response = iReq.getResponse();
+
+        // Parse the message
+        if(method == HttpMethod.GET){
+        	final WorkflowModel[] models = WorkflowModel.fromJSONArray(response.getBody());
+        	System.err.println(models[0].getTitle());
+        	presenter.responseGet(models);
+        }
+        else{
+        	final WorkflowModel model = WorkflowModel.fromJson(response.getBody());
+        	System.err.println(model.getTitle());
+        	switch (method) {
+        	case GET:
+        		break;
+        	case POST:
+        		presenter.responsePost(model);
+        		break;
+        	case PUT:
+        		presenter.responsePut(model);
+        		break;
+        	case DELETE:
+        		presenter.responseDelete(model);
+        		break;
+        	}
+        }
+    }
+
+    /**
+     * @see edu.wpi.cs.wpisuitetng.network.RequestObserver#responseError(edu.wpi.cs.wpisuitetng.network.models.IRequest)
+     */
+    @Override
+    public void responseError(IRequest iReq) {
+        System.err.println("The request to " + httpMethodToString(method)
+                + " a workflow failed.");
+    }
+
+    /**
+	 * 
+	 */
+    @Override
+    public void fail(IRequest iReq, Exception exception) {
+        System.err.println("The request to " + httpMethodToString(method)
+                + " a workflow failed.");
+
+    }
+
+    /**
+     * @return The presenter
+     */
+    public WorkflowPresenter getPresenter() {
+        return this.presenter;
+    }
+
+    private static String httpMethodToString(HttpMethod method_) {
+        String methodString = "";
+        switch (method_) {
+        case GET:
+            methodString = "fetch";
+            break;
+        case POST:
+            methodString = "update";
+            break;
+        case PUT:
+            methodString = "create";
+            break;
+        case DELETE:
+            methodString = "delete";
+            break;
+        }
+        return methodString;
+    }
+}
