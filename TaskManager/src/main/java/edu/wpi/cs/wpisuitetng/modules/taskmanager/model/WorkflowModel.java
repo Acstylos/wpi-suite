c234f8709d4c18ee7891351891bbeb1bafbdff09
Yihao Zhou <yihaozhou1991@gmail.com>
2014-11-11 22:36:06 -0500

    final commit
    Change-Id: Ie1e763d291854e0522a31865839d8613664d8532

--- TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/model/WorkflowModel.java
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/model/WorkflowModel.java
@@ -5,91 +5,97 @@ import java.util.ArrayList;
 import com.google.gson.Gson;
 
 import edu.wpi.cs.wpisuitetng.modules.AbstractModel;
+
 /**
  * Workflow Model is the full screen (mainly the list of buckets)
  * @author TheFloorIsJava
  *
  */
 public class WorkflowModel extends AbstractModel{
+    private int ID;
+    private String workflowTitle;
+    private ArrayList <BucketModel> listOfBucket;
+    
+    public WorkflowModel(){
+	this("", -1);
+    }
+    
+    public WorkflowModel(WorkflowModel another){
+	this.ID = another.ID;
+	this.workflowTitle = another.workflowTitle;
+	this.listOfBucket = another.listOfBucket;
+    }
 
+    public WorkflowModel(String title, int ID){
+	this.ID = ID;
+	this.workflowTitle=title;
+	this.listOfBucket=new ArrayList<>();
 
-	private static int increment=1;
-	private int ID;
-	private String workflowTitle;
-	private ArrayList <BucketModel> listOfBucket;
-	
-	WorkflowModel(String title){
+    }
+    @Override
+    public void save() {
+	// TODO Auto-generated method stub
 
-		
-		this.ID=increment; 
-		this.workflowTitle=title;
-		this.listOfBucket=new ArrayList<BucketModel>();
-		WorkflowModel.increment++;
-	}
-	/**
-	 * Will Implement Later
-	 */
-	@Override
-	public void save() {
-		// TODO Auto-generated method stub
-		
-	}
+    }
 
-	/**
-	 * Will Implement Later
-	 */
-	@Override
-	public void delete() {
-		// TODO Auto-generated method stub
-		
-	}
-	/**
-	 * Passes JSON Server an instance of this Class.
-	 */
-	@Override
-	public String toJson() {
-		return new Gson().toJson(this, WorkflowModel.class);
+    @Override
+    public void delete() {
+	// TODO Auto-generated method stub
 
-	}
-	/**
-	 * Will Implement Later
-	 */
-	@Override
-	public Boolean identify(Object o) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	/**
-	 * gets the list of Buckets
-	 * @return the List of Buckets
-	 */
-	public ArrayList<BucketModel> getWorkflow(){
-		return this.listOfBucket;
-	}
-	
-	/**
-	 * get Title of WorkflowModel
-	 * @return workflowTitle
-	 */
-	public String getTitle(){
-		return this.workflowTitle;
-	}
-	
-	/**
-	 * set title of WorkflowModel
-	 * @param title
-	 */
-	public void setTitle(String title){
-		this.workflowTitle=title;
-	}
+    }
 
-	/**
-	 * returns the ID of this workflow Model
-	 * @return ID
-	 */
-	public int getID() {
-		return ID;
-	}
+    /**
+     * Returns an instance of WorkflowModel constructed using the given
+     * WorkflowModel encoded as a JSON string.
+     * 
+     * @param json the json-encoded WorkflowModel to deserialize
+     * @return the WorkflowModel contained in the given JSON
+     */
+    public static WorkflowModel fromJson(String json) {
+        final Gson parser = new Gson();
+        return parser.fromJson(json, WorkflowModel.class);
+    }
 
+    @Override
+    public String toJson() {
+	return new Gson().toJson(this, WorkflowModel.class);
 
+    }
+
+    @Override
+    public Boolean identify(Object o) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+    /**
+     * gets the list of Buckets
+     * @return the List of Buckets
+     */
+    public ArrayList<BucketModel> getWorkflow(){
+	return this.listOfBucket;
+    }
+
+    /**
+     * get Title of WorkflowModel
+     * @return workflowTitle
+     */
+    public String getTitle(){
+	return this.workflowTitle;
+    }
+
+    /**
+     * set title of WorkflowModel
+     * @param title
+     */
+    public void setTitle(String title){
+	this.workflowTitle=title;
+    }
+    
+    /**
+     * get the ID of WorkflowModel
+     * @return ID
+     */
+    public int getID(){
+	return this.ID;
+    }
 }
