b652d8ee98b56d0b35a2a172006f2997d870836c
Alex Rutfield <amrutfield@wpi.edu>
2014-11-17 19:37:01 -0500

    Add ActivityView for past comments and history

---
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/ActivityView.java
@@ -0,0 +1,77 @@
+package edu.wpi.cs.wpisuitetng.modules.taskmanager.view;
+
+import javax.swing.JPanel;
+
+import net.miginfocom.swing.MigLayout;
+
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+
+import java.awt.Color;
+
+import javax.swing.JButton;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+/*constructs message view for comments and history of tasks
+ * message: message within comment box
+ * 
+ */
+public class ActivityView extends JPanel {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 7743312885538872898L;
+    /**
+     * Create the panel.
+     */
+    private String message = "";
+    private JPanel panel;
+    private JPanel panel_1;
+    private JPanel pastMessagePanel;
+
+    public ActivityView() {
+        setLayout(new MigLayout("", "[grow]", "[]"));
+
+        pastMessagePanel = new JPanel();
+        pastMessagePanel.setForeground(Color.WHITE);
+        pastMessagePanel.setBackground(Color.LIGHT_GRAY);
+        add(pastMessagePanel, "cell 0 0,grow");
+        pastMessagePanel.setLayout(new MigLayout("", "[grow][]", "[]"));
+        this.pastMessagePanel = pastMessagePanel;
+        
+        
+
+
+    }
+
+    /*setMessage: sets the message within a MessageView and sets it up
+     * message: message within MessageView
+     */
+    public void setMessage(String message){
+        this.message = message;
+        JLabel lblNewLabel = new JLabel(message);
+        this.pastMessagePanel.add(lblNewLabel, "cell 0 0");
+        lblNewLabel.setForeground(Color.WHITE);
+        lblNewLabel.setBackground(Color.LIGHT_GRAY);
+
+    }
+    
+    /*Makes X button appear when mouse is over message
+     * (not yet working)
+     */
+    public void mouseEntered(MouseEvent e) {
+        JButton btnNewButton = new JButton("X");
+        pastMessagePanel.add(btnNewButton, "cell 1 0");
+        btnNewButton.addActionListener(new ActionListener() {
+        
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        });
+        
+     }
+}
+
--- TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/CommentView.java
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/CommentView.java
@@ -21,26 +21,33 @@ public class CommentView extends JPanel {
     private static final long serialVersionUID = 6161339014039149740L;
 
     public CommentView() {
-        setLayout(new MigLayout("", "[438px]", "[276px]"));
+        setLayout(new MigLayout("", "[grow]", "[grow]"));
 
         JTabbedPane commentPane = new JTabbedPane(JTabbedPane.TOP);
         add(commentPane, "cell 0 0,grow");
 
         JPanel commentPanel = new JPanel();
         commentPane.addTab("Comments", null, commentPanel, null);
-        commentPanel.setLayout(new MigLayout("", "[grow]",
-                "[60%,grow][grow][5%]"));
+        commentPanel.setLayout(new MigLayout("", "[grow][grow][23px]", "[grow][41px][23px]"));
 
         //scroll bar for box containing all comments
         JScrollPane commentScroll = new JScrollPane();
-        commentPanel.add(commentScroll, "cell 0 0,grow");
-
+        
+        commentPanel.add(commentScroll, "cell 0 0 3 1,grow");
+        
+        JPanel pastCommentpanel = new JPanel();
+        
+        pastCommentpanel.setLayout(new MigLayout("", "[grow]", "[grow]"));
+        ActivityView testActivity = new ActivityView();
+        pastCommentpanel.add(testActivity, "cell 0 0,alignx left,aligny top");
+        testActivity.setMessage("Hi John!");
+        commentScroll.setViewportView(pastCommentpanel);
         //scroll bar for box containing comment currently being edited
         JScrollPane editCommentScroll = new JScrollPane();
-        commentPanel.add(editCommentScroll, "cell 0 1,grow");
+        commentPanel.add(editCommentScroll, "cell 0 1 3 1,grow");
 
         //Write-in box for comments
-        JWriteinText commentText = new JWriteinText();
+        JWriteInText commentText = new JWriteInText();
         commentText.setStartText("Comment here");
         editCommentScroll.setViewportView(commentText);
 
@@ -50,14 +57,7 @@ public class CommentView extends JPanel {
             public void actionPerformed(ActionEvent e) {
             }
         });
-        commentPanel.add(btnPostComment,
-                "flowx,cell 0 2,alignx left,aligny center");
-
-        //Clear button (resets comment)
-        JButton btnClearComment = new JButton("Clear");
-        btnClearComment.addActionListener((ActionEvent e) -> {
-            commentText.resetText();
-        });
+        commentPanel.add(btnPostComment, "flowx,cell 0 2,alignx left,aligny center");
 
         // when clicked, the screen will clear if the original text is inside
         commentText.addMouseListener(new MouseAdapter() {
@@ -66,8 +66,16 @@ public class CommentView extends JPanel {
                 commentText.clicked();
             }
         });
-
-        commentPanel.add(btnClearComment, "cell 0 2");
+        
+                //Clear button (resets comment)
+                JButton btnClearComment = new JButton("Clear");
+                btnClearComment.addActionListener((ActionEvent e) -> {
+                    commentText.resetText();
+                });
+                
+                        commentPanel.add(btnClearComment, "cell 0 2,alignx left,aligny top");
+        
+        
         //history panel
         JPanel historyPanel = new JPanel();
         commentPane.addTab("History", null, historyPanel, null);
@@ -76,7 +84,14 @@ public class CommentView extends JPanel {
         //scroll bar for history
         JScrollPane historyScroll = new JScrollPane();
         historyPanel.add(historyScroll, "cell 0 0,grow");
-
+        
+        JPanel panel = new JPanel();
+        panel.setLayout(new MigLayout("", "[grow]", "[grow]"));
+        ActivityView testActivity2 = new ActivityView();
+        testActivity2.setMessage("Hello Alex!");
+        panel.add(testActivity2, "cell 0 0,alignx left,aligny top");
+        
+        historyScroll.setViewportView(panel);
     }
 
 }
