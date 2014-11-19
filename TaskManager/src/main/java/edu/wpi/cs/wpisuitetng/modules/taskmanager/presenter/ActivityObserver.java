ce1507bf341f131c643f3b1432505b0691ea2a51
unknown <sla@wpi.edu>
2014-11-19 00:55:02 -0500

    Updated Activity Presenter/Observer and View is temporary for now
    Change-Id: I53ef2886eae3fd42fb88d914f890c18bb2e8cf2f

--- TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/presenter/ActivityObserver.java
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/presenter/ActivityObserver.java
@@ -16,16 +16,19 @@ import edu.wpi.cs.wpisuitetng.network.models.HttpMethod;
 import edu.wpi.cs.wpisuitetng.network.models.IRequest;
 import edu.wpi.cs.wpisuitetng.network.models.ResponseModel;
 
+/**
+ * Observers the result of network requests for acivities
+ *
+ * @author TheFloorIsJava
+ */
 public class ActivityObserver implements RequestObserver {
 
-    private ActivityPresenter presenter;
-    private HttpMethod method;
+    ActivityPresenter presenter;
 
-    public ActivityObserver(ActivityPresenter presenter, HttpMethod method) {
-        this.presenter = presenter;
-        this.method = method;
-    }
-
+    /**
+     * Constructor for ActivityObserver.
+     * @param presenter ActivityPresenter
+     */
     public ActivityObserver(ActivityPresenter presenter) {
         this.presenter = presenter;
     }
@@ -33,79 +36,58 @@ public class ActivityObserver implements RequestObserver {
     /**
      * Parse the ActivityViews from the response received by the network
      *
+     * @param iReq IRequest
+     * @see edu.wpi.cs.wpisuitetng.network.RequestObserver#responseSuccess(IRequest)
      */
     public void responseSuccess(IRequest iReq) {
-        // Store the response
-        final ResponseModel response = iReq.getResponse();
+        /**
+        * Take the appropriate action based on what the method of the request
+        * was.
+        */
 
-        if (method == HttpMethod.GET) {
+       String json = iReq.getResponse().getBody();
+       ActivityModel model = new ActivityModel();
+       switch (iReq.getHttpMethod()) {
+       case GET:
+           model = ActivityModel.fromJsonArray(json)[0];
+           this.presenter.setModel(model);
+           this.presenter.updateView();
+           break;
+       case PUT:
+           model = ActivityModel.fromJson(json);
+           /*
+            * Set the new model and update the view to reflect the new data.
+            * GET and PUT requests both respond with a modified task - GET
+            * returns the task stored in the database and PUT returns the same
+            * task but with a new ID assigned.
+            */
+           this.presenter.setModel(model);
+           this.presenter.updateView();
+           break;
 
-            // Parse the message
-            final ActivityModel[] models = ActivityModel.fromJsonArray(response
-                    .getBody());
-            System.err.println(models[0].getActivity());
-            presenter.responseGet(models);
-        } else {
-            // Parse the message
-            final ActivityModel model = ActivityModel.fromJson(response
-                    .getBody());
-            switch (method) {
-            case GET:
-                break;
-            case POST:
-                presenter.responsePost(model);
-                break;
-            case PUT:
-                presenter.responsePut(model);
-                break;
-            case DELETE:
-                presenter.responseDelete(model);
-                break;
-            }
-        }
+       case POST:
+           break;
+
+       case DELETE:
+           break;
+       }
     }
 
     /**
      * @see edu.wpi.cs.wpisuitetng.network.RequestObserver#responseError(edu.wpi.cs.wpisuitetng.network.models.IRequest)
      */
     public void responseError(IRequest iReq) {
-        System.err.println("The request to " + httpMethodToString(method)
-                + " an activity failed.");
+        System.err.println(iReq.getResponse());
     }
 
     /**
      *
      * @param iReq
      * @param exception
+     * @see edu.wpi.cs.wpisuitetng.network.RequestObserver#fail(IRequest, Exception)
      */
     public void fail(IRequest iReq, Exception exception) {
-        System.err.println("The request to " + httpMethodToString(method)
-                + " an activity failed.");
-    }
-
-    private static String httpMethodToString(HttpMethod method_) {
-        String methodString = "";
-        switch (method_) {
-        case GET:
-            methodString = "fetch";
-            break;
-        case POST:
-            methodString = "update";
-            break;
-        case PUT:
-            methodString = "create";
-            break;
-        case DELETE:
-            methodString = "delete";
-            break;
-        }
-        return methodString;
-    }
-
-    /**
-     * @return The presenter
-     */
-    public ActivityPresenter getPresenter() {
-        return this.presenter;
+        System.err.println("Activity request failed");
+        exception.printStackTrace();
     }
 }
