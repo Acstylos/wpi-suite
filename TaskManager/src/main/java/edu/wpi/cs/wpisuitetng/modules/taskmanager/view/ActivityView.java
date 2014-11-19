3582d1e2e32e0e5eb974f5459c15c1376db54138
Stylos <a.stylos983@Gmail.com>
2014-11-18 23:52:27 -0500

    Fixes majority of bugs in Comments and Activity View.
    Change-Id: If66337c696aff57911c60a9f5604dcd8c396700f

--- TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/ActivityView.java
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/ActivityView.java
@@ -1,20 +1,11 @@
 package edu.wpi.cs.wpisuitetng.modules.taskmanager.view;
 
-import javax.swing.JPanel;
-
-import net.miginfocom.swing.MigLayout;
-
-import javax.swing.JTextArea;
-
 import java.awt.Color;
 
-import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.event.MouseEvent;
-import java.util.ArrayList;
-import java.util.List;
+import net.miginfocom.swing.MigLayout;
 /*constructs message view for comments and history of tasks
  * message: message within comment box
  * 
@@ -28,7 +19,6 @@ public class ActivityView extends JPanel {
     /**
      * Create the panel.
      */
-    private JPanel pastMessagePanel = new JPanel();
     private JTextArea pastActivityText = new JTextArea();
 
     /**
@@ -36,18 +26,26 @@ public class ActivityView extends JPanel {
      */
     public ActivityView() {
         this.setLayout(new MigLayout("", "[grow]", "[grow]"));
-        
-        pastActivityText.setEditable(false);
-        pastMessagePanel.setForeground(Color.WHITE);
-        pastMessagePanel.setBackground(Color.LIGHT_GRAY);
-        add(pastMessagePanel, "cell 0 0,grow");
-        pastMessagePanel.setLayout(new MigLayout("", "[grow]", "[grow]"));
-        this.pastMessagePanel.add(pastActivityText, "cell 0 0, grow");
-        pastActivityText.setForeground(Color.BLACK);
-        pastActivityText.setBackground(Color.LIGHT_GRAY);
+        this.add(pastActivityText, "cell 0 0, grow");
+        this.pastActivityText.setEditable(false);
+        this.pastActivityText.setForeground(Color.BLACK);
+        this.pastActivityText.setBackground(Color.LIGHT_GRAY);
         this.pastActivityText.setWrapStyleWord(true);
         this.pastActivityText.setLineWrap(true);
-        this.pastActivityText.setMinimumSize(this.getMinimumSize());
+    }
+    
+    /**
+     * Constructor sets up panel and colors
+     */
+    public ActivityView(String text) {
+        this.setLayout(new MigLayout("", "[grow]", "[grow]"));
+        this.add(pastActivityText, "cell 0 0, grow");
+        this.pastActivityText.setEditable(false);
+        this.pastActivityText.setForeground(Color.BLACK);
+        this.pastActivityText.setBackground(Color.LIGHT_GRAY);
+        this.pastActivityText.setWrapStyleWord(true);
+        this.pastActivityText.setLineWrap(true);
+        this.pastActivityText.setText(text);
     }
 
     /**
@@ -60,21 +58,5 @@ public class ActivityView extends JPanel {
     
     public String getMessage(){
         return this.pastActivityText.getText();
-    }
-
-    /**
-     * Makes X button appear when mouse is over message (not yet working)
-     * @param e: MouseEvent
-     */
-    public void mouseEntered(MouseEvent e) {
-        JButton deleteActivityButton = new JButton("X");
-        pastMessagePanel.add(deleteActivityButton, "cell 1 0");
-        deleteActivityButton.addActionListener(new ActionListener() {
-
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-
     }
 }
