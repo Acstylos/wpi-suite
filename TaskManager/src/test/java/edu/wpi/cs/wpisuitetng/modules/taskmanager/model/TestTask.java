dd82c9bf4028226fe408151b9dddd67a050dc1c4
Thomas Clark <the.thomas.j.clark@gmail.com>
2014-12-12 05:54:02 +0100

    Revert "Revert "Merge branch 'dev' of https://github.com/Acstylos/wpi-suite into feature/colors4""
    This reverts commit 873bae6e3ed80865e9ae4d1e080ec920f49b77e8.
    
    Change-Id: I4504a6f8059581326a610093dcf0417f509d72e8

--- TaskManager/src/test/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/model/TestTask.java
+++ TaskManager/src/test/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/model/TestTask.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Copyright (c) 2014 -- WPI Suite
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ ******************************************************************************/
 package edu.wpi.cs.wpisuitetng.modules.taskmanager.model;
 
 import static org.junit.Assert.*;
@@ -15,6 +24,7 @@ import edu.wpi.cs.wpisuitetng.modules.taskmanager.presenter.TaskPresenter;
 /**
  * Tests methods in iteration.java and tests the creation of variants of Bucket
  *
+ *@author theFloorIsJava
  */
 
 public class TestTask {
