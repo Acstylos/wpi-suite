87c7ea41190209539fc606370eb93a669554d7e2
Thomas Clark <tjclark@wpi.edu>
2014-12-14 02:34:25 -0500

    Add a servlet for sending email messages from the server
    The WPISuite server can now send email notifications to users when requested
    from the client through the standard REST API. A field to configure a
    per-project Gmail account to send from was also added.
    
    Anything in Janeway can send an email notification just by specifying a message
    and a list of users, without either having a hardcoded password in client code
    (only the admin might know the password) or a locally running MTA.
    
    There's nothing in the module to let you use it right now, so the only way
    to test it is with cURL. I'd like people to review this, but I won't submit it
    until there's client support for it.
    
    Change-Id: I3dcbc80d5dea6fddcf15c480369b56047b745db9

---
+++ Core/WPISuite/src/main/java/edu/wpi/cs/wpisuitetng/WPINotificationServlet.java
@@ -0,0 +1,88 @@
+/*******************************************************************************
+ * Copyright (c) 2014 -- WPI Suite
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package edu.wpi.cs.wpisuitetng;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.SimpleEmail;
+
+import edu.wpi.cs.wpisuitetng.modules.core.models.Notification;
+import edu.wpi.cs.wpisuitetng.modules.core.models.Project;
+
+/**
+ * Servelet for sending email notifications from WPISuite
+ * 
+ * Email notifications are sent by the server when a request is made from a
+ * client to notify other users about something. This hides the login details of
+ * the sending account from the client.
+ */
+@WebServlet("/notification")
+public class WPINotificationServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 270207518571957971L;
+
+    private static final String SMTP_HOST_NAME = "smtp.googlemail.com";
+    private static final int SMTP_PORT = 465;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        Notification notification = Notification.fromJSON(req.getReader()
+                .readLine());
+
+        System.out.println("Notification: " + notification);
+        System.out.println("Project: " + notification.getProject());
+
+        /* Get the secret authentication info for this project from the database */
+        String emailUsername, emailPassword;
+        try {
+            String projectIdNum = notification.getProject().getIdNum();
+            Project project = ManagerLayer.getInstance().getProjects()
+                    .getEntity(projectIdNum)[0];
+            emailUsername = project.getAutomaticEmailAddress();
+            emailPassword = project.getAutomaticEmailPassword();
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
+
+        try {
+            /*
+             * Send an email to the addresses marked as recipients of the
+             * notification
+             */
+            Email email = new SimpleEmail();
+            email.setAuthentication(emailUsername, emailPassword);
+            email.setSSLOnConnect(true);
+
+            email.setFrom(emailUsername, "WPISuite Task Manager");
+            email.setSubject(notification.getSubject());
+            email.setMsg(notification.getContent());
+            for (String recipient : notification.getRecipients()) {
+                email.addTo(recipient);
+            }
+
+            email.setHostName(SMTP_HOST_NAME);
+            email.setSmtpPort(SMTP_PORT);
+            email.send();
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
+    }
+}
