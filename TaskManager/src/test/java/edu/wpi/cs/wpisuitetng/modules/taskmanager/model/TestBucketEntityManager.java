bbab20756830007dbeb22822ea84bcee424c4f5d
Will <will.wen1@gmail.com>
2014-12-14 19:35:53 -0500

    fixed all the entity Managers yo

--- TaskManager/src/test/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/model/TestBucketEntityManager.java
+++ TaskManager/src/test/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/model/TestBucketEntityManager.java
@@ -12,6 +12,7 @@ package edu.wpi.cs.wpisuitetng.modules.taskmanager.model;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
 import java.util.HashSet;
 
 import org.junit.Before;
@@ -20,6 +21,7 @@ import org.junit.Test;
 import edu.wpi.cs.wpisuitetng.Session;
 import edu.wpi.cs.wpisuitetng.database.Data;
 import edu.wpi.cs.wpisuitetng.exceptions.BadRequestException;
+import edu.wpi.cs.wpisuitetng.exceptions.ConflictException;
 import edu.wpi.cs.wpisuitetng.exceptions.NotFoundException;
 import edu.wpi.cs.wpisuitetng.exceptions.WPISuiteException;
 import edu.wpi.cs.wpisuitetng.modules.core.models.Project;
@@ -29,20 +31,19 @@ import edu.wpi.cs.wpisuitetng.modules.taskmanager.MockData;
 import edu.wpi.cs.wpisuitetng.modules.taskmanager.model.BucketEntityManager;
 import edu.wpi.cs.wpisuitetng.modules.taskmanager.model.BucketModel;
 
-
 public class TestBucketEntityManager {
-	MockData db;
-	Session defaultSession;
-	Project testProject;
-	User testUser;
+    MockData db;
+    Session defaultSession;
+    Project testProject;
+    User testUser;
     String mockSsid;
-	BucketModel bkm;
-	BucketEntityManager manager;
-	
-	@Before
+    BucketModel bkm;
+    BucketEntityManager manager;
+
+    @Before
     public void setUp() {
-		db = new MockData(new HashSet<Object>());
-		testUser = new User("joe", "joe", "1234", 2);
+        db = new MockData(new HashSet<Object>());
+        testUser = new User("joe", "joe", "1234", 2);
         testUser.setRole(Role.ADMIN);
         testProject = new Project("test", "1");
         mockSsid = "abc123";
@@ -51,28 +52,38 @@ public class TestBucketEntityManager {
         manager = new BucketEntityManager(db);
         db.save(testUser);
     }
-	
-	/**
-	 * Test manager
-	 */
-	@Test
-	public void testMakeBucketEntity() {
-		assertNotNull(manager);
-	}
-    
+
+    /**
+     * Test manager
+     * 
+     * @throws WPISuiteException
+     * @throws ConflictException
+     * @throws BadRequestException
+     */
+    @Test
+    public void testMakeBucketEntity() throws BadRequestException,
+            ConflictException, WPISuiteException {
+        BucketModel test1 = new BucketModel(1, "dummyTitle");
+        test1.setProject(testProject);
+
+        assertNotNull(manager.makeEntity(defaultSession, test1.toJson()));
+    }
+
     /**
      * Test saving
-     * @throws WPISuiteException 
+     * 
+     * @throws WPISuiteException
      */
     @Test
     public void testSave() throws WPISuiteException {
         manager.save(defaultSession, new BucketModel());
     }
-    
+
     /**
      * Test count of buckets
      * 
-     * @throws WPISuiteException if not valid request
+     * @throws WPISuiteException
+     *             if not valid request
      */
     @Test
     public void testCount() throws WPISuiteException {
@@ -84,11 +95,12 @@ public class TestBucketEntityManager {
         manager.save(defaultSession, new BucketModel());
         assertEquals(3, manager.Count());
     }
-    
+
     /**
      * Test deleting all
      * 
-     * @throws WPISuiteException if not valid request
+     * @throws WPISuiteException
+     *             if not valid request
      */
     @Test
     public void testDeleteAllAndEnsureRole() throws WPISuiteException {
@@ -99,10 +111,11 @@ public class TestBucketEntityManager {
         manager.deleteAll(defaultSession);
         assertEquals(0, manager.Count());
     }
-    
+
     /**
      * Test getting all Buckets
-     * @throws WPISuiteException 
+     * 
+     * @throws WPISuiteException
      */
     @Test
     public void testGetAll() throws WPISuiteException {
@@ -112,9 +125,11 @@ public class TestBucketEntityManager {
         BucketModel bkmList[] = manager.getAll(defaultSession);
         assertEquals(3, bkmList.length);
     }
+
     /**
      * Test getting an entity
-     * @throws WPISuiteException 
+     * 
+     * @throws WPISuiteException
      */
     @Test
     public void testGetEntity() throws WPISuiteException {
@@ -122,7 +137,7 @@ public class TestBucketEntityManager {
         manager.save(defaultSession, new BucketModel(4, "test 4"));
         manager.save(defaultSession, new BucketModel(5, "test 5"));
         BucketModel bkmList[] = manager.getEntity(defaultSession, "4");
-        
+
         assertEquals(1, bkmList.length);
         assertEquals(4, bkmList[0].getId());
         assertEquals("test 4", bkmList[0].getTitle());
@@ -130,16 +145,19 @@ public class TestBucketEntityManager {
 
     /**
      * Test getting an invalid entity
-     * @throws WPISuiteException expects to not find the entity
+     * 
+     * @throws WPISuiteException
+     *             expects to not find the entity
      */
     @Test(expected = WPISuiteException.class)
     public void testGetBadEntity() throws WPISuiteException {
         manager.getEntity(defaultSession, "0");
     }
-    
+
     /**
      * Test getting an entity for an invalid iteration
-     * @throws WPISuiteException 
+     * 
+     * @throws WPISuiteException
      */
     @Test
     public void testGetEntityForBucketNotFound() throws WPISuiteException {
@@ -154,10 +172,12 @@ public class TestBucketEntityManager {
         }
         assertTrue(exceptionThrown);
     }
+
     /**
      * Test to delete an entity
      * 
-     * @throws WPISuiteException if not valid request
+     * @throws WPISuiteException
+     *             if not valid request
      */
     @Test
     public void testDeleteEntity() throws WPISuiteException {
@@ -173,34 +193,37 @@ public class TestBucketEntityManager {
         }
         assertTrue(exceptionThrown);
     }
-    
+
     /**
      * Test updating an iteration
      * 
-     * @throws WPISuiteException if not valid request
+     * @throws WPISuiteException
+     *             if not valid request
      */
     @Test
     public void testUpdatingAnInteration() throws WPISuiteException {
         manager.save(defaultSession, new BucketModel(3, "test 3"));
         assertEquals(1, manager.Count());
         assertEquals(3, manager.getEntity(defaultSession, "3")[0].getId());
-        
-        
-        assertEquals("test 3", manager.getEntity(defaultSession, "3")[0].getTitle());
 
-       manager.update(defaultSession, new BucketModel(3, "changed").toJson());
+        assertEquals("test 3",
+                manager.getEntity(defaultSession, "3")[0].getTitle());
+
+        manager.update(defaultSession, new BucketModel(3, "changed").toJson());
         assertEquals(1, manager.Count());
-        assertEquals("changed", manager.getEntity(defaultSession, "3")[0].getTitle());
+        assertEquals("changed",
+                manager.getEntity(defaultSession, "3")[0].getTitle());
 
         boolean exceptionThrown = false;
         try {
-            //this bucketModel is not saved yet , so will cause error
-            manager.update(defaultSession, new BucketModel(4, "change Id 4").toJson());
+            // this bucketModel is not saved yet , so will cause error
+            manager.update(defaultSession,
+                    new BucketModel(4, "change Id 4").toJson());
         } catch (WPISuiteException e) {
             exceptionThrown = true;
         }
-        //an exception will be thrown because the model was never saved, so cant update.
+        // an exception will be thrown because the model was never saved, so
+        // cant update.
         assertTrue(exceptionThrown);
     }
 }
-
