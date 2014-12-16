ae4c8949a922d6f01c049d1f8159378aad5948fb
Alex Rutfield <amrutfield@wpi.edu>
2014-12-16 01:25:48 -0500

    added expandable panel with functioning filters
    Authors: David Rubenstein, Alex Rutfield, Alex Stylos
    
    Change-Id: I7e7111edd9b42b4cf3f96dc5b7f50bf51040533f

--- TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/ToolbarView.java
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/ToolbarView.java
@@ -9,41 +9,20 @@
  ******************************************************************************/
 package edu.wpi.cs.wpisuitetng.modules.taskmanager.view;
 
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JToggleButton;
 import javax.swing.UIManager;
 
+import net.miginfocom.swing.MigLayout;
 import edu.wpi.cs.wpisuitetng.modules.taskmanager.presenter.BucketPresenter;
 import edu.wpi.cs.wpisuitetng.modules.taskmanager.presenter.TaskPresenter;
-import net.miginfocom.swing.MigLayout;
-
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.text.ParseException;
-import java.util.Date;
-import java.util.Map;
-
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
-import javax.swing.JComboBox;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
-import javax.swing.text.PlainDocument;
-
-import org.jdesktop.swingx.JXDatePicker;
-import org.jdesktop.swingx.JXTextField;
+import java.awt.Font;
 
 /**
  * Sets up upper toolbar of TaskManager tab
@@ -51,8 +30,10 @@ import org.jdesktop.swingx.JXTextField;
 public class ToolbarView extends JPanel
 {
     private static final long serialVersionUID = 5489162021821230861L;
-    private Date upperDateBound=null;
-    private Date lowerDateBound=null;
+    
+    private JButton createNewTaskButton = new JButton("<html>Create<br/>Task</html>", Icons.CREATE_TASK_LARGE);
+    private JToggleButton tglbtnArchive = new JToggleButton("<html>Hide<br/>Archived</html>", Icons.HIDE_ARCHIVE_LARGE);
+    private FilterView filterPanel = new FilterView();
     
     static {
         /* Change the default icons for JXDatePicker. */
@@ -66,13 +47,23 @@ public class ToolbarView extends JPanel
      * @throws IOException 
      */
     public ToolbarView() {
-        Color colorsFilterOptions[] = { new Color(238, 238, 238), Color.WHITE,
-                Color.YELLOW, Color.RED, Color.GREEN, Color.MAGENTA, Color.GRAY };
-        setLayout(new MigLayout("fill", "[grow][10%][10%][10%][10%]"));
+        setLayout(new MigLayout("", "[][][grow][]-5px", "0px[grow]"));
 
-        JButton createNewTaskButton = new JButton("<html>Create<br/>Task</html>");
-        createNewTaskButton.setIcon(Icons.CREATE_TASK_LARGE);
+        add(createNewTaskButton, "cell 0 0");
         
+        add(tglbtnArchive, "cell 1 0");
+        tglbtnArchive.setSelected(false);
+        
+        JLabel filterLabel = new JLabel("F I L T E R");
+        filterLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        add(filterLabel, "cell 2 0");
+        
+        
+        add(filterPanel, "cell 3 0,grow");
+        registerCallbacks();
+    }
+    
+    public void registerCallbacks(){
         
         /**
          * Adds a new TaskView Tab into the MainView
@@ -87,31 +78,10 @@ public class ToolbarView extends JPanel
                 MainView.getInstance().setSelectedIndex(tabCount-1);
             }
         });
-
-        add(createNewTaskButton, "flowx,cell 0 0");
         
-        JToggleButton tglbtnArchive = new JToggleButton("<html>Hide<br/>Archived</html>");
-        tglbtnArchive.setIcon(Icons.HIDE_ARCHIVE_LARGE);
-        tglbtnArchive.setSelected(true);
-        add(tglbtnArchive, "cell 0 0");
-
-        JComboBox filterColorComboBox = new JComboBox<Color>(
-                colorsFilterOptions);
-        filterColorComboBox.setRenderer(new ColorRenderer());
-        filterColorComboBox.setSelectedIndex(0);
-        filterColorComboBox.setSize(new Dimension(20, 5));
-        add(filterColorComboBox, "cell 1 0");
-        filterColorComboBox.addItemListener(new ItemListener() {
-
-            @Override
-            public void itemStateChanged(ItemEvent e) {
-                BucketPresenter.getTaskFilter().setFilterColor(
-                        (Color) filterColorComboBox.getSelectedItem());
-                MainView.getInstance().resetAllBuckets();
-            }
-
-        });
-
+        /**
+         * Hides or removes archives from the view.
+         */
         tglbtnArchive.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 if (tglbtnArchive.isSelected()) {
@@ -129,249 +99,6 @@ public class ToolbarView extends JPanel
                 MainView.getInstance().resetAllBuckets();
             }
         });
-        JXDatePicker filterStartDatePicker = new JXDatePicker();
-        JXDatePicker filterEndDatePicker = new JXDatePicker();
-        add(filterStartDatePicker, "cell 2 0");
-        add(filterEndDatePicker, "cell 3 0");
-        DocumentListener startDateListener = new DocumentListener() {
-            /** {@inheritDoc} */
-            @Override
-            public void changedUpdate(DocumentEvent e) {
-                filterStartDatePicker.getMonthView().setUpperBound(
-                        getUpperDateBound());
-                BucketPresenter.getTaskFilter().setStartDate(
-                        filterStartDatePicker.getDate());
-                setLowerDateBound(filterStartDatePicker.getDate());
-                MainView.getInstance().resetAllBuckets();
-            }
-
-            /** {@inheritDoc} */
-            @Override
-            public void insertUpdate(DocumentEvent e) {
-                filterStartDatePicker.getMonthView().setUpperBound(
-                        getUpperDateBound());
-                BucketPresenter.getTaskFilter().setStartDate(
-                        filterStartDatePicker.getDate());
-                setLowerDateBound(filterStartDatePicker.getDate());
-                MainView.getInstance().resetAllBuckets();
-
-            }
-
-            /** {@inheritDoc} */
-            @Override
-            public void removeUpdate(DocumentEvent e) {
-                filterStartDatePicker.getMonthView().setUpperBound(
-                        getUpperDateBound());
-                BucketPresenter.getTaskFilter().setStartDate(null);
-                setLowerDateBound(null);
-                MainView.getInstance().resetAllBuckets();
-            }
-
-        };
-        DocumentListener endDateListener = new DocumentListener() {
-            /** {@inheritDoc} */
-            @Override
-            public void changedUpdate(DocumentEvent e) {
-                filterEndDatePicker.getMonthView().setLowerBound(
-                        getLowerDateBound());
-                BucketPresenter.getTaskFilter().setEndDate(
-                        filterEndDatePicker.getDate());
-                setUpperDateBound(filterEndDatePicker.getDate());
-                MainView.getInstance().resetAllBuckets();
-            }
-
-            /** {@inheritDoc} */
-            @Override
-            public void insertUpdate(DocumentEvent e) {
-                filterEndDatePicker.getMonthView().setLowerBound(
-                        getLowerDateBound());
-                BucketPresenter.getTaskFilter().setEndDate(
-                        filterEndDatePicker.getDate());
-                setUpperDateBound(filterEndDatePicker.getDate());
-                MainView.getInstance().resetAllBuckets();
-
-            }
-
-            /** {@inheritDoc} */
-            @Override
-            public void removeUpdate(DocumentEvent e) {
-                filterEndDatePicker.getMonthView().setLowerBound(
-                        getLowerDateBound());
-                BucketPresenter.getTaskFilter().setEndDate(null);
-                setUpperDateBound(null);
-                MainView.getInstance().resetAllBuckets();
-            }
-
-        };
-        filterStartDatePicker.getEditor().getDocument()
-                .addDocumentListener(startDateListener);
-        filterEndDatePicker.getEditor().getDocument()
-                .addDocumentListener(endDateListener);
-        filterStartDatePicker.getEditor().addMouseListener(new MouseListener() {
-
-            @Override
-            public void mouseClicked(MouseEvent arg0) {
-                filterStartDatePicker.getMonthView().setUpperBound(
-                        getUpperDateBound());
-
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent arg0) {
-                filterStartDatePicker.getMonthView().setUpperBound(
-                        getUpperDateBound());
-
-            }
-
-            @Override
-            public void mouseExited(MouseEvent arg0) {
-                filterStartDatePicker.getMonthView().setUpperBound(
-                        getUpperDateBound());
-
-            }
-
-            @Override
-            public void mousePressed(MouseEvent arg0) {
-                filterStartDatePicker.getMonthView().setUpperBound(
-                        getUpperDateBound());
-
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent arg0) {
-                filterStartDatePicker.getMonthView().setUpperBound(
-                        getUpperDateBound());
-
-            }
-
-        });
-        filterEndDatePicker.getEditor().addMouseListener(new MouseListener() {
-
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                filterEndDatePicker.getMonthView().setLowerBound(
-                        getLowerDateBound());
-
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                filterEndDatePicker.getMonthView().setLowerBound(
-                        getLowerDateBound());
-
-            }
-
-            @Override
-            public void mouseExited(MouseEvent e) {
-                filterEndDatePicker.getMonthView().setLowerBound(
-                        getLowerDateBound());
-
-            }
-
-            @Override
-            public void mousePressed(MouseEvent e) {
-                filterEndDatePicker.getMonthView().setLowerBound(
-                        getLowerDateBound());
-
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                filterEndDatePicker.getMonthView().setLowerBound(
-                        getLowerDateBound());
-
-            }
-
-        });
-        JLabel startDateFilter = new JLabel("Filter By Start Date");
-        JLabel endDateFilter = new JLabel("Filter By End Date");
-        add(startDateFilter, "cell 2 1");
-        add(endDateFilter, "cell 3 1");
-
-        JXTextField filterByText = new JXTextField();
-        filterByText.setMinimumSize(new Dimension(100, 25));
-        add(filterByText, "cell 4 0");
-
-        filterByText.getDocument().addDocumentListener(new DocumentListener() {
-
-            @Override
-            public void changedUpdate(DocumentEvent e) {
-                BucketPresenter.getTaskFilter().setFilterText(filterByText.getText());
-                MainView.getInstance().resetAllBuckets();
-
-            }
-
-            @Override
-            public void insertUpdate(DocumentEvent e) {
-                BucketPresenter.getTaskFilter().setFilterText(filterByText.getText());
-                MainView.getInstance().resetAllBuckets();
-            }
-
-            @Override
-            public void removeUpdate(DocumentEvent e) {
-                BucketPresenter.getTaskFilter().setFilterText(filterByText.getText());
-                MainView.getInstance().resetAllBuckets();
-            }
-
-        });
-        JXTextField filterByUser = new JXTextField();
-        filterByUser.setMinimumSize(new Dimension(100, 25));
-        add(filterByUser, "cell 5 0");
-
-        filterByUser.getDocument().addDocumentListener(new DocumentListener() {
-
-            @Override
-            public void changedUpdate(DocumentEvent e) {
-                BucketPresenter.getTaskFilter().setFilterUser(filterByUser.getText());
-                MainView.getInstance().resetAllBuckets();
-            }
-
-            @Override
-            public void insertUpdate(DocumentEvent e) {
-                BucketPresenter.getTaskFilter().setFilterUser(filterByUser.getText());
-                MainView.getInstance().resetAllBuckets();
-            }
-
-            @Override
-            public void removeUpdate(DocumentEvent e) {
-                BucketPresenter.getTaskFilter().setFilterUser(filterByUser.getText());
-                MainView.getInstance().resetAllBuckets();
-            }
-
-        });
-        JLabel filterByTextLabel = new JLabel("Filter By Text Fields");
-        JLabel filterByUserLabel = new JLabel("Filter By Users");
-        add(filterByTextLabel, "cell 4 1");
-        add(filterByUserLabel, "cell 5 1");
-
+        
     }
-
-    /**gets highest date allowed by start date
-     * @return upperDateBound highest date allowed
-     */
-    private Date getUpperDateBound() {
-        return upperDateBound;
-    }
-
-    /**sets highest date allowed
-     * @param upperDateBound highest date allowed by start date
-     */
-    private void setUpperDateBound(Date upperDateBound) {
-        this.upperDateBound = upperDateBound;
-    }
-
-    /**gets lowest date allowed by finish date
-     * @return lowest date allowed
-     */
-    private Date getLowerDateBound() {
-        return lowerDateBound;
-    }
-
-    /*sets lowest date allowed
-     * @param lowerDateBound lowest date allowed by finish date
-     */
-    private void setLowerDateBound(Date lowerDateBound) {
-        this.lowerDateBound = lowerDateBound;
-    }
-
 } 
